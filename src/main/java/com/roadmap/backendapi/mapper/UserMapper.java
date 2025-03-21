e2fb07fabb5a284ec15dc7196f789d38494718db
Sherif Elglaly <sherifelglaly@gmail.com>
2025-03-21 06:49:47 +0200

    refactor(mapper): use mapstruct for entity mapping
    Migrates from ModelMapper to MapStruct for DTO/Entity conversion.
    
    - Improves type safety and performance.
    - Removes ModelMapper dependency.
    - Simplifies mapper implementations.

--- src/main/java/com/roadmap/backendapi/mapper/UserMapper.java
+++ src/main/java/com/roadmap/backendapi/mapper/UserMapper.java
@@ -1,31 +1,17 @@
 package com.roadmap.backendapi.mapper;
 
 import com.roadmap.backendapi.dto.UserDTO;
-import com.roadmap.backendapi.model.User;
-import lombok.RequiredArgsConstructor;
+import com.roadmap.backendapi.entity.User;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
-@Component
+@Mapper(componentModel = "spring")
+public interface UserMapper {
 
-public class UserMapper implements IUserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserDTO toDTO(User user);
 
 
-    private ModelMapper modelMapper;
-
-    public UserMapper(ModelMapper modelMapper) {
-        this.modelMapper = modelMapper;
-    }
-
-    @Override
-    public UserDTO toDTO(User user) {
-        return modelMapper.map(user, UserDTO.class);
-    }
-
-    @Override
-    public User toEntity(UserDTO userDTO) {
-        return modelMapper.map(userDTO, User.class);
-    }
 }
