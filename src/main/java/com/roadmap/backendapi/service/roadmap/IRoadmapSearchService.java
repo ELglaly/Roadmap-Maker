51ad79b0b96f068a9ecff8173ba5e9cdd7a744a1
Sherif Elglaly <sherifelglaly@gmail.com>
2025-03-21 06:50:51 +0200

    feat(roadmap): refactor roadmap service to use interfaces
    - Introduce RoadmapManagementService and RoadmapSearchService interfaces to separate concerns.
    - Implement RoadmapService with RoadmapServiceImpl.
    - Update related classes to use new interfaces.
    
    Refactor the RoadmapService to improve maintainability and separation of concerns by introducing RoadmapManagementService and RoadmapSearchService interfaces. This allows for a more modular and testable design. RoadmapServiceImpl is introduced to implement those two interfaces. The old RoadmapService interface is now extended by two interfaces to manage the roadmap.

--- src/main/java/com/roadmap/backendapi/service/roadmap/IRoadmapSearchService.java
+++
@@ -1,9 +0,0 @@
-package com.roadmap.backendapi.service.roadmap;
-
-import com.roadmap.backendapi.dto.RoadmapDTO;
-
-public interface IRoadmapSearchService {
-    RoadmapDTO getRoadmapById(Long roadmapId);
-    RoadmapDTO getRoadmapByUserId(Long userId);
-    RoadmapDTO getRoadmapByTitle(String title);
-}
