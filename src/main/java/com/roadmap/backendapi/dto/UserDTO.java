95388c89e9b3115d292e16c6a96354f78290fd24
Sherif Elglaly <sherifelglaly@gmail.com>
2025-03-30 03:29:11 +0200

    feat(user): add role and enabled fields to UserDTO
    Adds role and enabled attributes to the UserDTO.
    
    - Include role for authorization.
    - Include enabled status for account activation.
    - Enables better user management.

--- src/main/java/com/roadmap/backendapi/dto/UserDTO.java
+++ src/main/java/com/roadmap/backendapi/dto/UserDTO.java
@@ -19,6 +19,8 @@ public class UserDTO {
     private String email;
     private String username;
     private String password;
+    private String role;
+    private boolean enabled;
     private String goal;
     private String interests;
     private String skills ;
