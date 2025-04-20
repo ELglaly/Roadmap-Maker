bfa28922776e099bc2271d972f06ba7cdf0e6572
Sherif Elglaly <sherifelglaly@gmail.com>
2025-04-20 19:15:43 +0200

    feat(consts): add password error messages

---
+++ src/main/java/com/roadmap/backendapi/Consts.java
@@ -0,0 +1,14 @@
+package com.roadmap.backendapi;
+
+public class Consts {
+
+    public static class PasswordErrorMessage
+    {
+        public static final String PASSWORD_TOO_SHORT = "Password must be at least 8 characters long";
+        public static final String PASSWORD_REQUIRED = "Password is required";
+        public static final String PASSWORD_MISMATCH = "Passwords do not match";
+        public static final String PASSWORD_INCORRECT = "Password is incorrect";
+        public static final String PASSWORD_CONSTRAINS = "Password must contain at least one digit, one lowercase letter, one uppercase letter, one special character, and no whitespace";
+
+    }
+}
