6738fe3fe924ceee3c8497dc17bc7777d50aa062
Sherif Elglaly <sherifelglaly@gmail.com>
2025-04-22 03:20:32 +0200

    feat(progress): add request fields
    Adds fields to Create/Update Progress Requests
    
    - Add userId, milestoneId, completed_at to CreateProgressRequest
    - Add userId, milestoneId, progressId, completed_at to UpdateProgressRequest

--- src/main/java/com/roadmap/backendapi/request/progress/UpdateProgressRequest.java
+++ src/main/java/com/roadmap/backendapi/request/progress/UpdateProgressRequest.java
@@ -1,4 +1,15 @@
 package com.roadmap.backendapi.request.progress;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Setter
+@Getter
 public class UpdateProgressRequest {
+    private Timestamp completed_at;
+    private Long userId;
+    private Long milestoneId;
+    private Long progressId;
 }
