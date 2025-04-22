bffe71f86366db1aba8e92fa76255e4df6ad65a6
Sherif Elglaly <sherifelglaly@gmail.com>
2025-04-22 03:34:17 +0200

    feat(websocket): enhance websocket functionality and error handling

--- src/main/java/com/roadmap/backendapi/exception/roadmap/RoadMapNotFoundException.java
+++ src/main/java/com/roadmap/backendapi/exception/roadmap/RoadMapNotFoundException.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class RoadMapNotFoundException extends AppException {
     public RoadMapNotFoundException() {
-
-      super("RESOURCE_NOT_FOUND",HttpStatus.CONFLICT);
+      super("RESOURCE_NOT_FOUND", HttpStatus.NOT_FOUND);
     }
 }
