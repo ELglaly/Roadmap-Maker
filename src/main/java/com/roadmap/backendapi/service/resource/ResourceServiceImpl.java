69dd82caf50d3e34e26953d94d4715487c70bb64
Sherif Elglaly <sherifelglaly@gmail.com>
2025-05-22 04:22:33 +0300

    fix(user): improve error handling for login

--- src/main/java/com/roadmap/backendapi/service/resource/ResourceServiceImpl.java
+++ src/main/java/com/roadmap/backendapi/service/resource/ResourceServiceImpl.java
@@ -43,7 +43,7 @@ public class ResourceServiceImpl implements ResourceService {
         } catch (ResourceAccessException e) {
             throw new ConnectionErrorException();
         }
-        catch (Exception e)
+        catch (RuntimeException e)
         {
             throw new RuntimeException(e.getMessage());
         }
