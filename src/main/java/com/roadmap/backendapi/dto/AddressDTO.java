ee563b0b26e237db3600f3a1e4c5bc754e53a4b1
Sherif Elglaly <sherifelglaly@gmail.com>
2025-08-07 14:33:13 +0300

    refactor(address): use Const utils
    Refactor the AddressDTO to use the Const utility class.
    
    - Replaced Consts with Const.RegularExpressions for pattern validation.
    - Updated zip code pattern to ZIP_PATTERN.

--- src/main/java/com/roadmap/backendapi/dto/AddressDTO.java
+++ src/main/java/com/roadmap/backendapi/dto/AddressDTO.java
@@ -1,6 +1,6 @@
 package com.roadmap.backendapi.dto;
 
-import com.roadmap.backendapi.Consts;
+import com.roadmap.backendapi.utils.Const;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -11,8 +11,8 @@ import java.io.Serializable;
  * DTO for {@link com.roadmap.backendapi.entity.Address}
  */
 public record AddressDTO(@Column(nullable = false) @NotBlank String street,
-                         @Column(length = 50, nullable = false) @NotBlank @Pattern(regexp = Consts.RegularExpression.STRING_PATTERN, message = Consts.RegularExpression.STRING_PATTERN_ERROR) String city,
-                         @Column(length = 15, nullable = false) @NotBlank @Pattern(regexp = Consts.RegularExpression.ZIB_PATTERN, message = Consts.RegularExpression.ZIB_PATTERN_ERROR) String zip,
-                         @Column(length = 50, nullable = false) @NotBlank @Pattern(regexp = Consts.RegularExpression.STRING_PATTERN, message = Consts.RegularExpression.STRING_PATTERN_ERROR) String country) implements Serializable {
+                         @Column(length = 50, nullable = false) @NotBlank @Pattern(regexp = Const.RegularExpressions.STRING_PATTERN, message = Const.RegularExpressions.STRING_PATTERN_ERROR) String city,
+                         @Column(length = 15, nullable = false) @NotBlank @Pattern(regexp = Const.RegularExpressions.ZIP_PATTERN, message = Const.RegularExpressions.ZIP_PATTERN_ERROR  ) String zip,
+                         @Column(length = 50, nullable = false) @NotBlank @Pattern(regexp = Const.RegularExpressions.STRING_PATTERN, message = Const.RegularExpressions.STRING_PATTERN_ERROR) String country) implements Serializable {
 
 }
